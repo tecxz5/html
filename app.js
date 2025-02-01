d7b2ec21af076ec550c77381b494d0918fa76573
tecxz5 <zamulatimofei@yandex.ru>
2025-02-01 21:57:10 +0300

    это ёбанный фарш

--- app.js
+++ app.js
@@ -5,18 +5,22 @@ const fs = require('fs');
 const app = express();
 const port = 3000;
 
+// Обслуживание статических файлов
 app.use(express.static(path.join(__dirname, 'site')));
 
+// Обработка маршрутов
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'site', 'pages', 'index', 'main', 'index.html'));
+});
+
 app.use((req, res, next) => {
-    // Если путь заканчивается на '/', добавляем 'index.html'
-    let filePath = path.join(__dirname, `${req.path}.html`);
-    
+    let filePath = path.join(__dirname, 'site', req.path);
+
     if (fs.existsSync(filePath)) {
-        // Если файл существует, отправляем его
         return res.sendFile(filePath);
     } else {
         // Если файл не найден, пробуем добавить 'index.html' по умолчанию
-        filePath = path.join(__dirname, `${req.path}/index.html`);
+        filePath = path.join(__dirname, 'site', req.path, 'index.html');
         
         if (fs.existsSync(filePath)) {
             return res.sendFile(filePath); // Отправляем индексный файл
@@ -27,21 +31,16 @@ app.use((req, res, next) => {
     }
 });
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'site', 'index.html'));
-});
-
 app.use((req, res) => {
-  const errorFilePath = path.join(__dirname, 'site', '404', '404.html');
+    const errorFilePath = path.join(__dirname, 'site', 'errors', '404', '404.html');
 
-  if (fs.existsSync(errorFilePath)) {
-      res.status(404).sendFile(errorFilePath);
-  } else {
-      res.status(404).send('Page not found');
-  }
+    if (fs.existsSync(errorFilePath)) {
+        res.status(404).sendFile(errorFilePath);
+    } else {
+        res.status(404).send('Page not found');
+    }
 });
-
 
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+    console.log(`Server is running at http://localhost:${port}`);
 });
